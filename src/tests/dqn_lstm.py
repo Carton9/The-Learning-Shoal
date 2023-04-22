06f1667e9d0be4a4dac398d7d317bbc6df40dc32
Scott Mayberry <sctmayberry@gmail.com>
2023-04-22 15:29:05 -0400

    added lstm version

--- src/tests/dqn_lstm.py
+++ src/tests/dqn_lstm.py
@@ -5,12 +5,9 @@ import torch.nn.functional as F
 
 class DQN_Basic(nn.Module):
     def __init__(self, observation_shape, n_actions):
-        # observation shape: (H,W,C) needs to be reshaped to (C,H,W)
-        # n_actions: int the size of actions
         super(DQN_Basic, self).__init__()
         self.n_actions = n_actions
         self.observation_shape = observation_shape
-        # rearrange input shape to (C, H, W)
         self.convolve_and_rearrange = nn.Sequential(
             nn.Conv2d(observation_shape[-1], 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
@@ -20,15 +17,17 @@ class DQN_Basic(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.flatten = nn.Flatten()
-        self.layer1 = nn.LazyLinear(128)
-        self.layer2 = nn.LazyLinear(self.n_actions)
-        self.softmax = nn.Softmax()
+        self.lstm = nn.LSTM(input_size=32, hidden_size=128, num_layers=1, batch_first=True)
+        self.layer1 = nn.Linear(128, 128)
+        self.layer2 = nn.Linear(128, self.n_actions)
+        self.softmax = nn.Softmax(dim=1)
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
-        x = self.convolve_and_rearrange(x.permute(0, 3, 1, 2))  # rearrange input shape to (C, H, W)
+        x = self.convolve_and_rearrange(x)
         x = self.flatten(x)
+        x = x.view(x.size(0), 1, -1)  # Reshape the tensor to (batch_size, 1, flattened_size) for LSTM input
+        x, _ = self.lstm(x)
+        x = x.squeeze(1)  # Remove the sequence dimension
         x = F.relu(self.layer1(x))
         x = self.layer2(x)
         x = self.softmax(x)
