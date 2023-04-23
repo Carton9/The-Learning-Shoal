50f032a23542448c3c7cc0fb2a706548601f9252
Mike cai <qazwqsx9@gmail.com>
2023-04-23 17:11:57 -0400

    Update on traning

--- src/tests/dqn_basic.py
+++ src/tests/dqn_basic.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import copy
 
 class DQN_Basic(nn.Module):
     def __init__(self, observation_shape, n_actions):
@@ -11,27 +12,35 @@ class DQN_Basic(nn.Module):
         self.n_actions = n_actions
         self.observation_shape = observation_shape
         # rearrange input shape to (C, H, W)
-        self.convolve_and_rearrange = nn.Sequential(
+        self.online = nn.Sequential(
             nn.Conv2d(observation_shape[-1], 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.LazyLinear(128),
+            nn.ReLU(),
+            nn.LazyLinear(self.n_actions),
+            nn.Softmax()
         )
-        self.flatten = nn.Flatten()
-        self.layer1 = nn.LazyLinear(128)
-        self.layer2 = nn.LazyLinear(self.n_actions)
-        self.softmax = nn.Softmax()
+        self.target = copy.deepcopy(self.online)
+        for p in self.target.parameters():
+            p.requires_grad = False
+        # self.flatten = nn.Flatten()
+        # self.layer1 = nn.LazyLinear(128)
+        # self.layer2 = nn.LazyLinear(self.n_actions)
+        # self.softmax = nn.Softmax()
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
+    def forward(self, x,model):
         # (1, H, W, C)
-        x = self.convolve_and_rearrange(x.permute(0, 3, 1, 2))  # rearrange input shape to (C, H, W)
-        x = self.flatten(x)
-        # print(x.shape)
-        x = F.relu(self.layer1(x))
-        x = self.layer2(x)
-        x = self.softmax(x)
+        # x = self.online(x.permute(0, 3, 1, 2))  # rearrange input shape to (C, H, W)
+        if model == "online":
+            x = self.online(x.permute(0, 3, 1, 2))
+        elif model == "target":
+            x = self.target(x.permute(0, 3, 1, 2))
         return x
+        
