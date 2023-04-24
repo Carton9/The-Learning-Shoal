b557658b406916727c5746856c4d7a7344e82bca
Scott Mayberry <sctmayberry@gmail.com>
2023-04-24 13:06:33 -0400

    minor edits

--- src/tests/scott_tests/dqn_net.py
+++ src/tests/scott_tests/dqn_net.py
@@ -20,13 +20,13 @@ class DQN_NET(nn.Module):
             # nn.BatchNorm2d(100),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1,padding=1),
             nn.LeakyReLU(0.01),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1,padding=1),
-            nn.LeakyReLU(0.01),
             # nn.BatchNorm2d(100),
             nn.Flatten(),
             nn.LazyLinear(256),
             nn.LeakyReLU(0.01),
             nn.LazyLinear(64),
+            nn.LeakyReLU(0.01),
+            nn.LazyLinear(32),
             nn.LeakyReLU(0.01),
             nn.LazyLinear(output_dim)
         )
