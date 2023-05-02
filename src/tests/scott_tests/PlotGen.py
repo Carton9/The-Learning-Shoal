e377ee9b3ea3cc542232f393a930e0310e35beac
Mike cai <qazwqsx9@gmail.com>
2023-05-01 20:54:21 -0400

    code

--- src/tests/scott_tests/PlotGen.py
+++ src/tests/scott_tests/PlotGen.py
@@ -2,18 +2,26 @@ import json
 import matplotlib.pyplot as plot
 import numpy as np
 from pathlib import Path
-folders=['Basic_With_AdvReward2','LSTM_With_Reward2',"Basic_With_Reward","2023-04-29T16-15-21"]
+import os
+
+folders=['2023-04-30T20-26-57','2023-04-30T20-21-05']
+name={'2023-04-30T20-26-57':'Basic_Edit_Reward','2023-04-30T20-21-05':'LSTM_Edit_Reward'}
 
 # path=Path("src\\tests\\scott_tests\\tiger_checkpoints") / "Basic_With_AdvReward2"/ "raw.json"
 # print(path)
 dataDict=None
-figFolder=Path("src\\tests\\scott_tests\\FinalPlot")
+figFolder=Path("FinalPlot")
 
 # print(dataDict.keys())
 for folder in folders:
     ylables={"moving_avg_ep_rewards":"rewards","moving_avg_ep_lengths":"steps","moving_avg_ep_avg_losses":"loss","moving_avg_ep_avg_qs":"Q value"}
-    path=Path("src\\tests\\scott_tests\\tiger_checkpoints") / folder/ "raw.json"
+    path=Path("deer_checkpoints") / folder/ "raw.json"
+    
     # path=Path("src\\tests\\scott_tests\\deer_checkpoints") / folder/ "raw.json"
+    # print(os.path.abspath(path))
+    
+    print(path)
+    input()
     with open(path, "r") as f:
         dataDict=json.load(f)
     for key in list(dataDict.keys())[4:]:
@@ -24,8 +32,9 @@ for folder in folders:
         plot.plot(dataDict[key][:700])
         plot.xlabel("episodes")
         plot.ylabel(ylables[key])
-        if folder =="2023-04-29T16-15-21":
-            plot.savefig(figFolder/("LSTM_With_AdvReward2"+"_tiger_"+key+'.png'))
-        else:
-            plot.savefig(figFolder/(folder+"_tiger_"+key+'.png'))
+        # if folder =="2023-04-29T16-15-21":
+        #     plot.savefig(figFolder/("LSTM_With_AdvReward2"+"_tiger_"+key+'.png'))
+        # else:
+        # plot.savefig(figFolder/(name[folder]+"_tiger_"+key+'.png'))
+        plot.savefig(figFolder/(name[folder]+"_deer_"+key+'.png'))
         plot.pause(1)
